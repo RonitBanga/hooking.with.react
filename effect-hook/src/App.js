2a100d7fa0b9b3b499aa4e7c2066a2d2fbe585e1
RonitBanga <bangaronit22@gmail.com>
2024-03-13 23:31:37 +0530

    useEffect Hook?

---
+++ effect-hook/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import HookCounterOne from './components/HookCounterOne';
+
+function App() {
+  return (
+    <div className="App">
+      <HookCounterOne />
+      <HookCounterOne />
+    </div>
+  );
+}
+
+export default App;
