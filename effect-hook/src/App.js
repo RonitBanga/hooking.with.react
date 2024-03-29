84f8bbf95fab9ead03262811f930c957ca75fed8
RonitBanga <bangaronit22@gmail.com>
2024-03-29 23:19:17 +0530

    useEffect Hook?

--- effect-hook/src/App.js
+++ effect-hook/src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import HookCounterOne from './components/HookCounterOne';
+import HookMouse from './components/HookMouse';
+import MouseContainer from './components/MouseContainer';
+import IntervalHookCounter from './IntervalHookCounter';
+import DataFetching from './components/DataFetching';
 
 function App() {
   return (
     <div className="App">
-      <HookCounterOne />
-      <HookCounterOne />
+      <DataFetching />
+
     </div>
   );
 }
